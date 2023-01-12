fa2a8923be9d577d2c667df869c6c17f0cca8ee8
sahil shaikh <jazim2211@gmail.com>
2023-01-12 19:37:05 +0530

    added for l

--- code/power_bi_report_refresh.ps1
+++ code/power_bi_report_refresh.ps1
@@ -1,7 +1,7 @@
 #setting home path
 echo "running script is very good"
 
-cd ..\test
+cd ..\test #directory where all pbix is present
 $file_list=$(Get-ChildItem -name)
 
 Write-Host "file list is $file_list"
@@ -9,4 +9,8 @@ Write-Host "file list is $file_list"
 foreach ($x in $file_list)
 {
   Write-Host "this is printing $x"
+  $report_name=$($x.split(".")[-2])
+
+  Write-Host "command run here to update dataset $report_name "
+
 }
