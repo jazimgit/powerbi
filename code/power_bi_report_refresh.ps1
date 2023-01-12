864237ff38d4b1ac44045ddc486c5c21af817be5
sahil shaikh <jazim2211@gmail.com>
2023-01-12 20:46:02 +0530

    added every thing

--- code/power_bi_report_refresh.ps1
+++ code/power_bi_report_refresh.ps1
@@ -4,16 +4,16 @@ echo "running script is very good"
 
 
 cd ..\test #directory where all pbix is present
+
+
 $file_list=$(Get-ChildItem -name)
-
 Write-Host "file list is $file_list"
-
 foreach ($x in $file_list)
 {
-  Write-Host "this is printing $x"
+  Write-Host "here add upload command $y"
   $report_name=$($x.split(".")[-2])
 
-  Write-Host "command run here to update dataset $report_name "
+  Write-Host "here run script to update dataset_id $report_name"
 
 }
 
@@ -21,9 +21,9 @@ $comma_file="file1.pbix"
 $comma_list=$comma_file.Split(",")
 foreach ($y in $comma_list)
 {
-  Write-Host "this is printing $y"
+  Write-Host "here add upload command $y"
   $report_name=$($y.split(".")[-2])
 
-  Write-Host "command run here to update dataset $report_name"
+  Write-Host "here run script to update dataset_id $report_name"
 
 }
