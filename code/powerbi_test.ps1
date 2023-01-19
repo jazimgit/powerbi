8d9586d3027509ff3dd4df076a604abec26f08a0
sahil shaikh <jazim2211@gmail.com>
2023-01-19 15:29:13 +0530

    added every thing

---
+++ code/powerbi_test.ps1
@@ -0,0 +1,21 @@
+param (
+    [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$false)]
+    $ENV,
+
+    [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$false)]
+    $Report_name,
+
+    [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$false)]
+    $Gateway_Connection
+
+    [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$false)]
+    $WORKSPACE
+
+)
+
+Write-Host $ENV
+Write-Host $Report_name
+Write-Host $Gateway_Connection
+Write-Host "jenkins home $WORKSPACE"
+
+
